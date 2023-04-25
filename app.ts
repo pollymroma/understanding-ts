c3080b2033758ae29858098aabe561851983ffb6
pollymroma <romanellopm@gmail.com>
2023-04-25 00:32:02 -0300

    object type practice

--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("Your code goes here...");
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  //TS infers the type, knows that the object has a key name string and a key age number
+  name: "Maximilian",
+  age: 30,
+};
+
+console.log(person.nickname); //a property that doesn't exist
+//It infers that the type of person object has a key name with type string and an age key with type number
